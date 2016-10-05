45deb6f8448cbab3822f38ddabd631eb963ea4d4
Tien Nguyen <tiennguyen.ftu.k52@gmail.com>
2016-10-05 21:09:21 +0700

    Thich thi commit

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.use(function (req, res, next) {
-    if (req.headers['x-forwarded-proto']) return next();
-    res.redirect('http://' + req.hostname + req.url);
+app.use(function (req, res, next){
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
 });
 
 app.use(express.static(__dirname + '/public'));
