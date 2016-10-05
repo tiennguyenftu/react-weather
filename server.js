039a8685f4a42554ccfe2f754b1358647901c855
Tien Nguyen <tiennguyen.ftu.k52@gmail.com>
2016-10-05 20:58:53 +0700

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.use(function (req, res, next) {
+    if (req.header['x-forwarded-proto']) return next();
+    res.redirect('http://' + req.hostname + req.url);
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT || 3000, function () {
