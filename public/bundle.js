a3dd9aa6989b1664211e7e1f8a8a518e144504a4
Tien Nguyen <tiennguyen.ftu.k52@gmail.com>
2016-10-05 21:13:12 +0700

    Rename examples page

--- public/bundle.js
+++ public/bundle.js
@@ -28692,9 +28692,18 @@
 
 	var Examples = function Examples(props) {
 	    return React.createElement(
-	        'h3',
+	        'div',
 	        null,
-	        'Examples Component'
+	        React.createElement(
+	            'h3',
+	            null,
+	            'Examples'
+	        ),
+	        React.createElement(
+	            'p',
+	            null,
+	            'Welcome to examples page'
+	        )
 	    );
 	};
 
