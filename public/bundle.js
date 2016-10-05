7f87b08ff0a71085fa88bcb464b779fb50c14e0e
Tien Nguyen <tiennguyen.ftu.k52@gmail.com>
2016-10-05 23:12:54 +0700

    Style about page

--- public/bundle.js
+++ public/bundle.js
@@ -28776,23 +28776,52 @@
 /* 270 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 
 	var React = __webpack_require__(8);
 
 	var About = function About(props) {
 	    return React.createElement(
-	        'div',
+	        "div",
 	        null,
 	        React.createElement(
-	            'h3',
-	            null,
-	            'About'
+	            "h1",
+	            { className: "text-center" },
+	            "About"
 	        ),
 	        React.createElement(
-	            'p',
+	            "p",
 	            null,
-	            'Welcome to the about page.'
+	            "This is a weather application build on React. I build this for complete React Developer Course."
+	        ),
+	        React.createElement(
+	            "p",
+	            null,
+	            "Here are some of the tools I used:"
+	        ),
+	        React.createElement(
+	            "ul",
+	            null,
+	            React.createElement(
+	                "li",
+	                null,
+	                React.createElement(
+	                    "a",
+	                    { href: "https://facebook.github.io/react" },
+	                    "React"
+	                ),
+	                " - This was the Javascript framework used."
+	            ),
+	            React.createElement(
+	                "li",
+	                null,
+	                React.createElement(
+	                    "a",
+	                    { href: "http://openweathermap.org" },
+	                    "Open Weather Map"
+	                ),
+	                " - I used Open Weather Map to search for weather at cities."
+	            )
 	        )
 	    );
 	};
