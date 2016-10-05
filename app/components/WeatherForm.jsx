09deab6c1832f633e292c09a8ef1ba3416adc6f3
Tien Nguyen <tiennguyen.ftu.k52@gmail.com>
2016-10-05 20:26:40 +0700

    first commit

---
+++ app/components/WeatherForm.jsx
@@ -0,0 +1,26 @@
+var React = require('react');
+
+var WeatherForm = React.createClass({
+
+    onFormSubmit: function (e) {
+        e.preventDefault();
+        var location = this.refs.location.value;
+        if (location.length) {
+            this.refs.location.value = '';
+            this.props.onSearch(location);
+        }
+    },
+
+    render: function () {
+        return (
+            <div>
+                <form onSubmit={this.onFormSubmit}>
+                    <input type="text" placeholder="Enter city name" ref="location"/>
+                    <input type="submit" value="Get Weather"/>
+                </form>
+            </div>
+        )
+    }
+});
+
+module.exports = WeatherForm;
